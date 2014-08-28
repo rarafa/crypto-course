88b091c7c39b08233660c5e36273bbf18b9bdbb7
rarafa <rafawts@gmail.com>
2014-08-27 20:56:20 -0500

    Update onetimepad.cpp

--- onetimepad.cpp
+++ onetimepad.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h> //rename(), remove() files
 using namespace std;
 
-void generateFiles(int padLen, int keys, const char* filename1,  const char* filename2){
+int generateFiles(int padLen, int keys, const char* filename1,  const char* filename2){
   int i,j, val;
   fstream file1, file2;
   try{
@@ -15,7 +15,9 @@ void generateFiles(int padLen, int keys, const char* filename1,  const char* fil
 	srand(time(NULL));
 	for(i=0; i<keys; i++){
 	  for(j=0; j<padLen; j++){
-		val = rand()%95; //ASCII printable characters range from 32 to 126
+		/*ASCII printable characters for keys (range 32-126), it
+		  actually doesn't matter, but it helps for visualizing the key files*/
+		val = rand()%95; 
 		val+=32;
 		file1<<(char)val;
 		file2<<(char)val; 
@@ -28,7 +30,7 @@ void generateFiles(int padLen, int keys, const char* filename1,  const char* fil
   }
   catch(...){
 	cerr<<"File I/O error";
-	exit(0);
+	return -1;
   }
 }
 
@@ -56,30 +58,31 @@ string oneTimePad(string input, const char* keysFilename){
 	tmpFile.close();
 	keys.close();
 	if( remove(keysFilename) != 0){
-	  throw;
+	  throw "File I/O error";
 	}
 	if(	rename(tmpFilename, keysFilename) != 0){
-	  throw;
+	  throw "File I/O error";
 	}
   }
-  catch(...){
-	cerr<<"File I/O error";
+  catch(const char* e){
+	cerr<<e;
 	exit(0);
   }
   return cipher;
 }
 
-void session(int padLen, int keys, const char* filename1, const char* filename2){
+int session(int padLen, int keys, const char* filename1, const char* filename2){
   //simulates two users' interaction
   string  plainT, cipherT, decipherT;
-  int i,j;
+  int plainTLen;
   while( (keys--) > 0){
 	cout<<"Message for Alice:\t";
-	cin>>plainT;
-	for(int i=0; i<plainT.length(); i++){
+	getline(cin, plainT);
+	plainTLen = plainT.length();
+	for(int i=0; i<plainTLen; i++){
 	  if(!isprint(plainT[i])){ 
 		cerr<<"Please enter only ASCII printable characters";
-		exit(0);
+		return -1;
 	  }
 	}
 	cipherT = oneTimePad(plainT, filename1);
@@ -87,6 +90,7 @@ void session(int padLen, int keys, const char* filename1, const char* filename2)
 	decipherT = oneTimePad(cipherT, filename2);
 	cout<<"Alice deciphers:\t"<<decipherT<<"\n\n";
   }
+  return 0;
 }
 
 int main(int argc, char **argv){
@@ -97,19 +101,26 @@ int main(int argc, char **argv){
 
   if(argc<3){
 	cerr<<"Usage:\t"<<argv[0]<<" <number of keys> <length of keys>";
-	exit(0);
+	return -1;
   }
   try{
 	tmp.str(argv[1]);
-	tmp>>keys;
+	if( !(tmp>>keys) ) throw "Specify number of keys";
 	tmp.clear();
 	tmp.str(argv[2]);
-	tmp>>padLen;
-  }catch(...){
-	cerr<<"Usage:\t"<<argv[0]<<" <number of keys> <length of keys>";
-	exit(0);
+	if( !(tmp>>padLen) ) throw "Specify pad length";
+	if (keys < 1 || padLen < 1) throw "Specify positive values";
   }
-  generateFiles(padLen, keys, filename1, filename2);
-  session(padLen, keys, filename1, filename2);
+  catch(const char* e){
+	cerr<<e<<"\n";
+	cerr<<"Usage:\t"<<argv[0]<<" <number of keys> <length of keys>";
+	return -1;
+  }
+  if( !(generateFiles(padLen, keys, filename1, filename2))){
+	return -1;
+  }
+  if( !(session(padLen, keys, filename1, filename2))){
+	return -1;
+  }
   return 0;
 }
